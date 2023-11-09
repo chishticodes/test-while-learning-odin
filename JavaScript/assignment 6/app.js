fc63ea883e3453375fa52cef38e674b85629949e
chishticodes <chishticodes@gmail.com>
2023-11-09 07:06:50 +0500

    assignment 6 - Basic for...of and simple for loop was written

--- JavaScript/assignment 6/app.js
+++ JavaScript/assignment 6/app.js
@@ -1,7 +1,20 @@
-// simple for of loop
+// simple for...of loop
 
 const friends = ['hasan','sarim','Minhaj','Sajjad','hassan','mateen']
 
 for (friend of friends){
     console.log(`My friend is ${friend}`);
+}
+
+// simple for loop with a condtitional statement
+let sentence = "My friends are "
+for (let i = 0; i<=friends.length;i++){
+    if(i == friends.length-1){
+        sentence += `and ${friends[i]}`;
+        console.log(sentence);
+    }else if(i == friends.length-2){
+        sentence+=`${friends[i]} `
+    }else{
+        sentence+=`${friends[i]}, `
+    }
 }
